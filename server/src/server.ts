390569334fb83719b62596cc49000d6915da5a07
Eliezer Soares <eliezerdrum@gmail.com>
2020-06-04 10:49:24 -0300

    updated

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import express, { response } from 'express';
+import express, { response, request } from 'express';
 
 const app = express();
 
@@ -15,14 +15,32 @@ const app = express();
 // GET: http://localhost:3333/users = Listar usuários
 // GET: http://localhost:3333/users/5 = Buscar dados do usuário com ID: 5
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+// Request Param: São parâmetros que vem na própria rota que identificam um usuário
+// Query Param: São parâmetros que vem na propria rota geralmente opcionais para filtros e paginações
+// Request Body: Parâmetros para criação/atualização de informações
 
-    response.json([
-        'Diego',
-        'Eliezer',
-        'Paloma'
-    ]);
+
+const users = [
+    'Diego',
+    'Eliezer',
+    'Paloma',
+    'Luiz'   
+];
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
 });
 
 app.post('/users', (request, response) => {
