f10e261db9f4b34482350555a0d0f3969f0eff8d
Eliezer Soares <eliezerdrum@gmail.com>
2020-06-06 12:47:33 -0300

    updated

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -10,8 +10,8 @@ const itemsController = new ItemsController();
 routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
-// Index, show, create, update, delete
 
 export default routes;
