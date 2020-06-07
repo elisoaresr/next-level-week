710a6324754443e55a91eb192280244cde3db4f5
Eliezer Soares <eliezerdrum@gmail.com>
2020-06-07 17:09:11 -0300

    Web Application

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,78 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+
+
+import './styles.css'
+
+import logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
-    return <h1>Hello Planet</h1>
+  return (
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta" />
+
+        <Link to="/">
+          <FiArrowLeft />
+                    Voltar para home
+        </Link>
+      </header>
+
+      <form>
+        <h1>Cadastro do <br />ponto de coleta</h1>
+
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="email">Nome da entidade</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+            />
+          </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="whatsapp">Whatsapp</label>
+              <input
+                type="text"
+                name="name"
+                id="whatsapp"
+              />
+            </div>
+
+
+          </div>
+
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+          </legend>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Ítens de coleta</h2>
+          </legend>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default CreatePoint;
