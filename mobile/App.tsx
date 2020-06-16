deff15e5cb600788b9584b008f9168861db987f8
Eliezer Soares <eliezerdrum@gmail.com>
2020-06-16 18:19:35 -0300

    Web Application

---
+++ mobile/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StatusBar, View } from 'react-native';
+
+import Home from './src/pages/home/index';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Home />
+    </>  
+  );
+}
